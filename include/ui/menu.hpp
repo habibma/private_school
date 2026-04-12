425c6d3d12bdba7d30a6fe1cff015f932c908cb9
habibma <habib.motavassel@gmail.com>
2026-04-12 15:46:27 +0200

    To seprate decleration and implementation

--- include/ui/menu.hpp
+++ include/ui/menu.hpp
@@ -13,35 +13,8 @@ struct MenuOption {
 
 class Menu {
     public:
-        Menu(const std::string& title, const std::vector<MenuOption>& options)
-            : title(title), options(options) {}
-
-        void display() const {
-            while (true) {
-                cout << "\n=== " << title << " ===\n";
-
-                for (size_t i = 0; i < options.size(); ++i) {
-                    cout << i + 1 << ". " << setw(10) << left << options[i].label
-                         << " - " << options[i].description
-                         << "\n";
-                }
-
-                cout << "> ";
-
-                int choice;
-                if (cin >> choice) {
-                    if (choice >= 1 && choice <= (int)options.size()) {
-                        cin.ignore(1000, '\n');
-                        options[choice - 1].action();
-                        return;
-                    }
-                }
-
-                cout << "Invalid choice. Try again. Please use the number corresponding to your choice.\n";
-                cin.clear();
-                cin.ignore(1000, '\n');
-            }
-        }
+        Menu(const std::string& title, const std::vector<MenuOption>& options);
+        void    display() const;
 
     private:
         std::string title;
